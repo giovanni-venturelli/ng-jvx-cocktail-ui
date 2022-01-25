77f6ee306027825f621f07a3d0b22efb565d8e20
giovanni.venturelli <giovanni.venturelli@digitronica.it>
2022-01-25 17:35:29 +0100

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,15 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import {NgJvxCocktailUiModule} from '../../projects/ng-jvx-cocktail-ui/src/public-api';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    NgJvxCocktailUiModule
   ],
   providers: [],
   bootstrap: [AppComponent]
